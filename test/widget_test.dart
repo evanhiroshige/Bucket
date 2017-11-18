57165d82ce2e193618c93cae65ee2e09d9c6eeb4
JMichaelYang <jaewonmichael@gmail.com>
2017-11-18 13:01:46 -0500

    Folders

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/main.dart';
+import 'package:bucket_list/ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
